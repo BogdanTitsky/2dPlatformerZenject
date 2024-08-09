516bcb77df79a4c479923afa9599cb7b34161ca9
Bogdan Titsky <bogdan.titsky@gmail.com>
2024-08-09 19:27:00 +0300

    StaticDataStart

---
+++ Assets/CodeBase/StaticData/GoblinStaticData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CodeBase.StaticData
+{
+    [CreateAssetMenu(fileName = "GoblinData", menuName = "StaticData/GoblinData")]
+    public class GoblinStaticData : ScriptableObject
+    {
+        public string TypeId;
+        
+        [Range(1, 100)]
+        public int Hp;
+        
+        [Range(1, 999)]
+        public int Damage;
+
+        [Range(0.5f, 1)]
+        public float EffectiveDistance;
+        [Range(0.5f, 1)]
+        public float Cleavage;
+
+        public GameObject Prefab;
+    }
+}
