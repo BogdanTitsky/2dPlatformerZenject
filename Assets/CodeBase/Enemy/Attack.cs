4e0171a8a391ba49f845c6863d3660b78cd38034
Bogdan Titsky <bogdan.titsky@gmail.com>
2024-08-13 08:51:03 +0300

    enemyAttackTrigger

---
+++ Assets/CodeBase/Enemy/Attack.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class Attack : MonoBehaviour
+    {
+        [SerializeField] private float cleavage = 1f;
+        [SerializeField] private float range = 1f;
+        [SerializeField] private int layerMask;
+        [SerializeField] private float attackCooldown = 1.5f;
+        [SerializeField] private EnemyAnimator animator;
+        
+        
+        private Collider2D[] _hits = new Collider2D[1];
+        private float _currentAttackCooldown;
+        private bool _isAttacking;
+        private bool _attackIsActive;
+
+
+        private void Awake()
+        {
+            layerMask = 1 << LayerMask.NameToLayer("Player");
+        }
+
+        private void Update()
+        {
+            UpdateCooldown();
+            if (CanAttack())
+                StartAttack();
+        }
+
+        public void EnableAttack() => 
+            _attackIsActive = true;
+
+        public void DisableAttack() => 
+            _attackIsActive = false;
+
+        private void StartAttack()
+        {
+            animator.PlayAttack();
+            _isAttacking = true;
+        }
+        
+        private void OnAttack()
+        {
+            if (Hit(out Collider2D hit))
+            {
+               PhysicsDebug.DrawDebug(StartPoint(), cleavage, 2);
+            }
+        }
+        
+        private void OnAttackEnd()
+        {
+            _currentAttackCooldown = attackCooldown;
+            _isAttacking = false;
+        }
+
+        private void UpdateCooldown()
+        {
+            if (!CooldownIsUp())
+                _currentAttackCooldown -= Time.deltaTime;
+        }
+
+        private bool Hit(out Collider2D hit)
+        {
+            Vector2 startPoint = StartPoint();
+            
+            Vector2 size = Size();
+
+            int hitCount = Physics2D.OverlapCapsuleNonAlloc(startPoint, size,
+                CapsuleDirection2D.Vertical,  layerMask,_hits);
+
+            hit = _hits.FirstOrDefault();
+
+            return hitCount > 0;
+        }
+
+        private Vector2 Size()
+        {
+            float radius = cleavage / 2; 
+            float height = cleavage;
+            Vector2 size = new Vector2(radius, height);
+            return size;
+        }
+
+        private Vector2 StartPoint()
+        {
+            return new Vector2(transform.position.x, transform.position.y) +
+                   new Vector2(transform.localScale.x, transform.localScale.y) * range;
+        }
+        
+        private bool CanAttack() => 
+            !_isAttacking && CooldownIsUp() && _attackIsActive;
+
+        private bool CooldownIsUp() => 
+            _currentAttackCooldown <= 0;
+
+        
+    }
+}
---
+++ Assets/CodeBase/Enemy/CheckAttackRange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(Attack))]
+    public class CheckAttackRange : MonoBehaviour
+    {
+        [SerializeField] private Attack attack;
+        [SerializeField] private TriggerObserver triggerObserver;
+
+        private void OnEnable()
+        {
+            triggerObserver.TriggerEnter += TriggerEnter;
+            triggerObserver.TriggerExit += OnTriggerExit;
+            
+            attack.DisableAttack();
+        }
+        
+        private void OnDisable()
+        {
+            triggerObserver.TriggerEnter -= TriggerEnter;
+            triggerObserver.TriggerExit -= OnTriggerExit;
+        }
+        
+        private void TriggerEnter(Collider2D obj)
+        {
+            attack.EnableAttack();
+        }
+
+        private void OnTriggerExit(Collider2D obj)
+        {
+            attack.DisableAttack();
+
+        }
+    }
+}
