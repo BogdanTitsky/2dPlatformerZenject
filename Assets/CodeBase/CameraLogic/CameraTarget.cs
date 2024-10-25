c827cbf097e49bbf3f9ba610a4a6b39cdb789535
Bogdan Titsky <bogdan.titsky@gmail.com>
2024-10-25 14:27:01 +0300

    LevelExtended

--- Assets/CodeBase/CameraLogic/CameraTarget.cs
+++ Assets/CodeBase/CameraLogic/CameraTarget.cs
@@ -4,10 +4,11 @@ namespace CodeBase.CameraLogic
 {
     public class CameraTarget : MonoBehaviour
     {
-        [SerializeField] private int offset = 3;
+        [SerializeField] private int offset = 4;
         [SerializeField] private float cameraSpeed = 5;
         [SerializeField] private float maxSpeedMultiplier = 2f;
-        private  Camera _camera;
+        [SerializeField] private float minY = 1f;
+        private Camera _camera;
 
         private void Awake()
         {
@@ -18,9 +19,13 @@ namespace CodeBase.CameraLogic
         {
             Vector3 targetPosition = transform.position + new Vector3(0, offset, -10);
             float distance = Vector3.Distance(_camera.transform.position, targetPosition);
-            
+
             float speed = cameraSpeed + distance * maxSpeedMultiplier;
 
-            _camera.transform.position = Vector3.Lerp(_camera.transform.position, targetPosition, speed * Time.deltaTime); }
+            targetPosition.y = Mathf.Max(targetPosition.y, minY);
+
+            _camera.transform.position =
+                Vector3.Lerp(_camera.transform.position, targetPosition, speed * Time.deltaTime);
+        }
     }
 }
