2ce1b21df929dc37330bb964d9986634789047dd
Bogdan Titsky <bogdan.titsky@gmail.com>
2024-10-29 13:14:19 +0200

    Sign

---
+++ Assets/CodeBase/Environment/Sign.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace CodeBase.Environment
+{
+    public class Sign : MonoBehaviour
+    {
+        [SerializeField] private Collider2D collider;
+        [SerializeField] private GameObject textMessage;
+
+        private void Start() => textMessage.SetActive(false);
+
+        private void OnTriggerEnter2D(Collider2D other) => textMessage.SetActive(true);
+
+        private void OnTriggerExit2D(Collider2D other) => textMessage.SetActive(false);
+    }
+}
